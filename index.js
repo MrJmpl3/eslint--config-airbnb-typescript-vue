464cfd569395c2f58dff910d57e8ca5127efee7f
José Manuel Casani Guerra <jmpl3.soporte@gmail.com>
2021-07-16 15:41:43 -0500

    feat: first version

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021.
+ *
+ * File created to GLEN SOFT company.
+ */
+
+module.exports = {
+  extends: [
+    'eslint-config-airbnb-typescript-base',
+    require.resolve('./shared'),
+  ],
+};
